8ccbb35e9ce8732941b80a7200bdf512ce16ea17
wujinhu <wujinhu920@126.com>
2020-06-14 19:58:03 +0800

    make oss underfs depends on hadoop-aliyun

--- underfs/oss/src/main/java/alluxio/underfs/oss/OSSUnderFileSystemFactory.java
+++ underfs/oss/src/main/java/alluxio/underfs/oss/OSSUnderFileSystemFactory.java
@@ -11,60 +11,25 @@
 
 package alluxio.underfs.oss;
 
-import alluxio.AlluxioURI;
 import alluxio.Constants;
-import alluxio.conf.PropertyKey;
-import alluxio.underfs.UnderFileSystem;
 import alluxio.underfs.UnderFileSystemConfiguration;
-import alluxio.underfs.UnderFileSystemFactory;
-
-import com.google.common.base.Preconditions;
-import com.google.common.base.Throwables;
-
-import java.io.IOException;
+import alluxio.underfs.hdfs.HdfsUnderFileSystemFactory;
 
 import javax.annotation.concurrent.ThreadSafe;
 
 /**
- * Factory for creating {@link OSSUnderFileSystem}.
+ * Factory for creating {@link alluxio.underfs.hdfs.HdfsUnderFileSystem}.
  */
 @ThreadSafe
-public class OSSUnderFileSystemFactory implements UnderFileSystemFactory {
-
-  /**
-   * Constructs a new {@link OSSUnderFileSystemFactory}.
-   */
-  public OSSUnderFileSystemFactory() {}
-
-  @Override
-  public UnderFileSystem create(String path, UnderFileSystemConfiguration conf) {
-    Preconditions.checkNotNull(path, "path");
-
-    if (checkOSSCredentials(conf)) {
-      try {
-        return OSSUnderFileSystem.createInstance(new AlluxioURI(path), conf);
-      } catch (Exception e) {
-        throw Throwables.propagate(e);
-      }
-    }
-
-    String err = "OSS Credentials not available, cannot create OSS Under File System.";
-    throw Throwables.propagate(new IOException(err));
-  }
+public class OSSUnderFileSystemFactory extends HdfsUnderFileSystemFactory {
 
   @Override
   public boolean supportsPath(String path) {
     return path != null && path.startsWith(Constants.HEADER_OSS);
   }
 
-  /**
-   * @param conf optional configuration object for the UFS
-   *
-   * @return true if both access, secret and endpoint keys are present, false otherwise
-   */
-  private boolean checkOSSCredentials(UnderFileSystemConfiguration conf) {
-    return conf.isSet(PropertyKey.OSS_ACCESS_KEY)
-        && conf.isSet(PropertyKey.OSS_SECRET_KEY)
-        && conf.isSet(PropertyKey.OSS_ENDPOINT_KEY);
+  @Override
+  public boolean supportsPath(String path, UnderFileSystemConfiguration conf) {
+    return supportsPath(path);
   }
 }
